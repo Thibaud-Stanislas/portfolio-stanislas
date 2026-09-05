24d612f10fb55832029fdee37ad26c34a66ea06f
stanislas thibaud <109161200+Thibaud-Stanislas@users.noreply.github.com>
2026-09-05 15:51:29 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://thibaud-stanislas.github.io',
+  base: '/portfolio-stanislas',
+});
